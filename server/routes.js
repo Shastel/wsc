80ebc0b6bd72d05522a35db737beff87cb363779
Max Shastel <max.shastik@yandex.ru>
2016-09-10 18:04:49 +0300

    Send bundles file on any req

--- server/routes.js
+++ server/routes.js
@@ -1,18 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/rules', function(req, res, next) {
+router.get('/rules', function(req, res) {
 
 });
 
-if (process.env.NODE_ENV === 'development') {
-    router.all('*', function(req, res, next){
-
-    });
-}
-
-router.get('*', function(req, res, next){
-    
+router.get('*', function(req, res){
+    res.sendFile('../client/dist/index.html');
 });
 
 module.exports = router;
