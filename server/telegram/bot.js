a4aaccd11a8fb42453d3858963757801e586eff9
Max Shastel <max.shastik@yandex.ru>
2016-11-15 00:19:05 +0300

    add telegram api key

--- server/telegram/bot.js
+++ server/telegram/bot.js
@@ -2,7 +2,7 @@ import TeleBot from 'telebot';
 import controller from 'server/telegram/controller';
 
 const bot = new TeleBot({
-    token: '253414327:AAGwmQrhj4jSczm3erdWAUBWtAUyBDqn1yw', // Required. Telegram Bot API token.
+    token: process.env.TELEGRAM_API_KEY, // Required. Telegram Bot API token.
     pooling: { // Optional. Use pooling.
         interval: 1000, // Optional. How often check updates (in ms).
         timeout: 0, // Optional. Update pulling timeout (0 - short polling).
